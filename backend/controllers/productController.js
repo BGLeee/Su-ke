d1aa7c5760b66d0b8a0e8c939a0e53f8b397d66f
BGLeee <birukgelila309@gmal.com>
2024-11-03 15:45:58 +0300

    add global error handler and middleware for error

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 const mongoose = require("mongoose");
-
+const ErrorHandler = require("../utils/errorHandler");
 // Create new product => /api/v1/product/new
 try {
   exports.newProduct = async (req, res, next) => {
@@ -47,10 +47,12 @@ exports.getSingleProduct = async (req, res, next) => {
     const product = await Product.findById(productId);
 
     if (!product) {
-      return res.status(404).json({
-        success: false,
-        message: "Product not found",
-      });
+      return next(new ErrorHandler("Product not found", 404));
+
+      // res.status(404).json({
+      //   success: false,
+      //   message: "Product not found",
+      // });
     }
     res.status(200).json({
       success: true,
